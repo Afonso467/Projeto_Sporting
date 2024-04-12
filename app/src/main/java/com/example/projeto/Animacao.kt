409799dc1097aa52b7e03c2877c673cf6e580027
David Catarino <93658234+DCatarino05@users.noreply.github.com>
2024-04-12 12:55:13 +0100

    .

--- app/src/main/java/com/example/projeto/Animacao.kt
+++ app/src/main/java/com/example/projeto/Animacao.kt
@@ -1,10 +1,13 @@
 package com.example.projeto
 
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -14,8 +17,10 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.splash_screen)
 
         Handler(Looper.getMainLooper()).postDelayed({
+            
+
             startActivity(Intent(this, Login::class.java))
             finish()
-        }, 4000)
+        }, 5000)
     }
 }
