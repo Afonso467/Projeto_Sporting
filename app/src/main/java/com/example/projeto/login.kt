9c98f32ef9ae796dfbb958069dc85d34fd9d65c5
David Catarino <93658234+DCatarino05@users.noreply.github.com>
2024-04-12 11:53:05 +0100

    .

--- app/src/main/java/com/example/projeto/login.kt
+++ app/src/main/java/com/example/projeto/login.kt
@@ -1,27 +1,15 @@
 package com.example.projeto
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
 
-        val handler = Handler(Looper.getMainLooper())
-
-
-        handler.postDelayed({
-            startActivity(Intent(this, Login::class.java))
-            finish()
-
-        }, 1500)
     }
 }
-
-
