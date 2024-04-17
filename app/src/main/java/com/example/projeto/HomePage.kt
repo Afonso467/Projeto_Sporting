cc0b5c0a65002fc61cf0c9041f7d728f63d83749
David Catarino <93658234+DCatarino05@users.noreply.github.com>
2024-04-17 16:16:47 +0100

    '

---
+++ app/src/main/java/com/example/projeto/HomePage.kt
@@ -0,0 +1,42 @@
+package com.example.projeto
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+
+class HomePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homepage)
+
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+
+        // Configurando o adapter para o ViewPager
+        val adapter = MyPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        // Configurando notícias (apenas exemplo, você precisará substituir por suas próprias notícias)
+        val newsList = listOf(
+            News("Título da Notícia 1", "Conteúdo da Notícia 1"),
+            News("Título da Notícia 2", "Conteúdo da Notícia 2"),
+            News("Título da Notícia 3", "Conteúdo da Notícia 3"),
+            News("Título da Notícia 4", "Conteúdo da Notícia 4")
+        )
+
+        val linearLayout = findViewById<LinearLayout>(R.id.LinearLayoutNews)
+        for (news in newsList) {
+            val newsView = layoutInflater.inflate(R.layout.item_news, null)
+            val titleTextView = newsView.findViewById<TextView>(R.id.textViewTitle)
+            val contentTextView = newsView.findViewById<TextView>(R.id.textViewContent)
+            titleTextView.text = news.title
+            contentTextView.text = news.content
+            linearLayout.addView(newsView)
+        }
+    }
+}
+
+data class News(val title: String, val content: String)
